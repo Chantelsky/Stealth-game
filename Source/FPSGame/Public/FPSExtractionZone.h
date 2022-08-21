31b3196529c5f548edc3e043c79ab41e46305f34
Chantel Punt <chantelsky@protonmail.com>
2022-08-21 23:03:56 +1000

    Setup post-mission camera

--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -20,11 +20,14 @@ public:
 protected:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
-		UBoxComponent* OverlapComp;
+	UBoxComponent* OverlapComp;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UDecalComponent* DecalComp;
 
 	UFUNCTION()
-		void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Sounds")
+	USoundBase* ObjectiveMissingSound;
 };
